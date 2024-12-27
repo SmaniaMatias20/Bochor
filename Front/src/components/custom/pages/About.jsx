d872e7ae7ac981ec5bf7b6ce9023e79f21977232
Matias <matiasezequielsmania@gmail.com>
2024-12-27 17:39:21 -0500

    TimeLine

--- Front/src/components/custom/pages/About.jsx
+++ Front/src/components/custom/pages/About.jsx
@@ -7,8 +7,8 @@ export function About() {
     return (
         <section className="flex flex-col items-center min-h-screen w-full bg-white p-0 md:p-0 lg:p-0">
             <Employees />
-            <MissionVisionValues />
             <Timeline />
+            <MissionVisionValues />
         </section>
     );
 }

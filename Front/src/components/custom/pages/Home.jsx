3b8e531cc698a20260fc9f944009d7ec6af94bab
Matias <matiasezequielsmania@gmail.com>
2024-12-27 21:50:09 -0500

    eventos

--- Front/src/components/custom/pages/Home.jsx
+++ Front/src/components/custom/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner";
 import Clients from "../Clients";
+import CalendarEvents from "../CalendarEvents";
 
 export function Home() {
   return (
     <section className="flex flex-col items-center min-h-screen w-full bg-white p-0 md:p-0 lg:p-0">
       <Banner />
+      <CalendarEvents />
       <Clients />
     </section>
   );

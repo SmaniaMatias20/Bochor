fbb1efbe71be03e8931c96656e635fa3dad71dfd
Matias <matiasezequielsmania@gmail.com>
2025-01-03 16:28:19 -0500

    redes sociales en las cartas

--- Front/src/components/custom/Employees.jsx
+++ Front/src/components/custom/Employees.jsx
@@ -1,8 +1,8 @@
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '../shadcn/card';
-import employee from "../../img/empleado.png";
 import employee1 from "../../img/empleado1.jpg";
 import employee2 from "../../img/empleado2.jpg";
 import employee3 from "../../img/empleado3.jpg";
+import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa'; // Importando iconos de FontAwesome
 
 const Employees = () => {
     const employees = [
@@ -10,19 +10,34 @@ const Employees = () => {
             name: "Juan Pérez",
             position: "Gerente de Recursos Humanos",
             description: "Encargado de liderar el equipo de recursos humanos, responsable de la gestión de talento y desarrollo organizacional.",
-            image: employee3
+            image: employee3,
+            socialLinks: {
+                facebook: "https://facebook.com/juanperez",
+                twitter: "https://twitter.com/juanperez",
+                linkedin: "https://linkedin.com/in/juanperez"
+            }
         },
         {
             name: "María González",
             position: "Coordinadora de Capacitación",
             description: "Responsable de la planificación y ejecución de programas de capacitación para el desarrollo profesional del personal.",
-            image: employee1
+            image: employee1,
+            socialLinks: {
+                facebook: "https://facebook.com/mariagonzalez",
+                twitter: "https://twitter.com/mariagonzalez",
+                linkedin: "https://linkedin.com/in/mariagonzalez"
+            }
         },
         {
             name: "Carlos Rodríguez",
             position: "Consultor Senior",
             description: "Consultor experimentado en la gestión de procesos de selección y asesoramiento estratégico para empresas.",
-            image: employee2
+            image: employee2,
+            socialLinks: {
+                facebook: "https://facebook.com/carlosrodriguez",
+                twitter: "https://twitter.com/carlosrodriguez",
+                linkedin: "https://linkedin.com/in/carlosrodriguez"
+            }
         },
     ];
 
@@ -60,11 +75,22 @@ const Employees = () => {
                         <CardContent className="p-4">
                             <p className="text-gray-700 text-sm">{employee.description}</p>
                         </CardContent>
+                        {/* Footer con redes sociales */}
+                        <div className="flex justify-center space-x-4 p-4">
+                            <a href={employee.socialLinks.facebook} target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-blue-600">
+                                <FaFacebook size={20} />
+                            </a>
+                            <a href={employee.socialLinks.twitter} target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-blue-400">
+                                <FaTwitter size={20} />
+                            </a>
+                            <a href={employee.socialLinks.linkedin} target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-blue-700">
+                                <FaLinkedin size={20} />
+                            </a>
+                        </div>
                     </Card>
                 ))}
             </div>
         </section>
-
     );
 };
 

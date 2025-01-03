3857ba99edcd30a5741c779cd9d99c06a596b3d0
Matias <matiasezequielsmania@gmail.com>
2025-01-03 16:36:16 -0500

    boton de wpp

--- Front/src/components/custom/WhatsAppButton.jsx
+++ Front/src/components/custom/WhatsAppButton.jsx
@@ -8,7 +8,7 @@ const WhatsAppButton = () => {
                 href="https://wa.me/541162559205"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-green-500 text-white py-3 px-3 rounded-full shadow-lg flex items-center"
+                className="bg-green-500 text-white py-2 px-2 rounded-full shadow-lg flex items-center"
             >
                 <FaWhatsapp className="h-8 w-8" />
             </a>
